d5c02efb031320383aba0998c0696623873d1278
Davide Bianco <dn.bianco03@gmail.com>
2023-05-14 15:13:04 +0200

    shared: Export CompleterGroup

--- lib/dahlia_shared.dart
+++ lib/dahlia_shared.dart
@@ -7,6 +7,7 @@ export 'services/locale.dart';
 export 'services/preferences.dart';
 export 'services/service.dart';
 
+export 'utils/completer_group.dart';
 export 'utils/constants.dart';
 export 'utils/extensions.dart';
 export 'utils/log.dart';
