39942335acf0d1dca267bf04dafbbcf63fac323e
Lars <larsb24@outlook.com>
2023-05-14 15:48:30 +0200

    Customization and ZenitUI Theme - Added Zenit to pubspec - switched to a Zenit based theme - removed laucherIcon, fontFamily and compactLauncher from CustomizationService - addded a .gitignore... smh

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -14,129 +14,33 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import 'package:animations/animations.dart';
-import 'package:flutter/material.dart';
 import 'package:dahlia_shared/services/customization.dart';
 import 'package:dahlia_shared/utils/constants.dart';
-import 'package:dahlia_shared/utils/extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:zenit_ui/zenit_ui.dart';
 
-ThemeData buildDahliaTheme(BuildContext context) {
+ThemeData get dahliaLightTheme {
   final CustomizationService customization = CustomizationService.current;
-
-  final bool darkMode = customization.darkMode;
-
-  final Color accentColor =
-      customization.accentColor.resolve() ?? BuiltinColor.orange.value;
-  final Color foregroundColor = context.theme.foregroundColor;
-  final Color backgroundColor = darkMode ? Colors.black : Colors.white;
-  final Color surfaceForegroundColor = darkMode ? Colors.white : Colors.black;
-  final Color surfaceColor = Color(darkMode ? 0xff1E1E1E : 0xffffffff);
-  final Color cardColor = Color(darkMode ? 0xFF2c2c2c : 0xFFEBEBEB);
-
-  final String fontFamily = customization.fontFamily;
-
-  final Brightness brightness = darkMode ? Brightness.dark : Brightness.light;
-
-  return ThemeData(
-    pageTransitionsTheme: const PageTransitionsTheme(
-      builders: {
-        TargetPlatform.android:
-            FadeThroughPageTransitionsBuilder(fillColor: Colors.transparent),
-      },
-    ),
-    hoverColor: context.theme.colorScheme.surface.withOpacity(0.25),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      elevation: 0.0,
-      hoverElevation: 0.0,
-      focusElevation: 0.0,
-      highlightElevation: 0.0,
-      foregroundColor: foregroundColor,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(
-          accentColor,
-        ),
-        elevation: MaterialStateProperty.all(0.0),
-        foregroundColor: MaterialStateProperty.all(
-          foregroundColor,
-        ),
-      ),
-    ),
-    appBarTheme: AppBarTheme(
-      color: accentColor,
-      elevation: 0.0,
-      iconTheme: IconThemeData(color: foregroundColor),
-    ),
-    fontFamily: fontFamily,
-    canvasColor: surfaceColor,
+  final Color accentColor = customization.accentColor.resolve() ?? BuiltinColor.orange.value;
+  return ThemeEngine.create(
+    variant: ThemeVariant.light,
     primaryColor: accentColor,
-    primaryColorDark: accentColor,
-    cardColor: cardColor,
-    scaffoldBackgroundColor: surfaceColor,
-    inputDecorationTheme: InputDecorationTheme(
-      labelStyle: TextStyle(
-        color: accentColor,
-      ),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide(
-          color: accentColor,
-          width: 2,
-        ),
-      ),
-    ),
-    listTileTheme: ListTileThemeData(
-      dense: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-    ),
-    //TODO Text Theme
-    textTheme: const TextTheme(
-      labelLarge: TextStyle(fontSize: 13),
-      /* displaySmall: TextStyle(fontSize: 11),
-      displayMedium: TextStyle(fontSize: 12),
-      displayLarge: TextStyle(fontSize: 13),
-      //body
-      bodySmall: TextStyle(fontSize: 11),
-      bodyMedium: TextStyle(fontSize: 12),
-      bodyLarge: TextStyle(fontSize: 13),
-      //label
-      labelSmall: TextStyle(fontSize: 11),
-      labelMedium: TextStyle(fontSize: 12),
-      labelLarge: TextStyle(fontSize: 13),
-      //title
-      titleSmall: TextStyle(fontSize: 11),
-      titleMedium: TextStyle(fontSize: 12),
-      titleLarge: TextStyle(fontSize: 13),
+    backgroundColor: const Color(0xFFFAFAFA),
+    surfaceColor: const Color(0xFFE5E5E7),
+    cardColor: const Color(0xFFFFFFFF),
+    textColor: Colors.black,
+  );
+}
 
-      //headline
-      headlineSmall: TextStyle(fontSize: 11),
-      headlineMedium: TextStyle(fontSize: 12),
-      headlineLarge: TextStyle(fontSize: 13), */
-    ),
-    dialogBackgroundColor: backgroundColor,
-    platform: TargetPlatform.fuchsia,
-    sliderTheme: SliderThemeData(
-      overlayColor: accentColor.op(0.1),
-      thumbColor: accentColor,
-      activeTrackColor: accentColor,
-      inactiveTrackColor: accentColor.op(0.5),
-      activeTickMarkColor: Colors.white.op(0.2),
-      inactiveTickMarkColor: Colors.white.op(0.2),
-    ),
-    iconTheme: IconThemeData(
-      color: surfaceForegroundColor,
-    ),
-    colorScheme: ColorScheme.fromSeed(
-      brightness: brightness,
-      seedColor: accentColor,
-      secondary: accentColor,
-      background: backgroundColor,
-    ),
+ThemeData get dahliaDarkTheme {
+  final CustomizationService customization = CustomizationService.current;
+  final Color accentColor = customization.accentColor.resolve() ?? BuiltinColor.orange.value;
+  return ThemeEngine.create(
+    variant: ThemeVariant.dark,
+    primaryColor: accentColor,
+    backgroundColor: const Color(0xFF1C1C1E),
+    surfaceColor: const Color(0xFF353535),
+    cardColor: const Color(0xFF252528),
+    textColor: Colors.white,
   );
 }
